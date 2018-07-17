3cb5578b82cea0b9ab44261ede4d3b2c985eae2a
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2018-07-17 11:50:31 +0200

    removed VLBI_Scheduler

--- VLBI_Scheduler/Earth.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by mschartn on 31.07.17.
-//
-
-#include "Earth.h"
-
-thread_local std::vector<double> VieVS::Earth::velocity{};
--- VLBI_Scheduler/VieVS_Object.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by mschartn on 12.03.18.
-//
-
-#include "VieVS_Object.h"
