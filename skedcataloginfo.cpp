b3d7a0af60a0bef8ae4988b67506f2dcb5e498ed
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2018-09-09 11:23:34 +0200

    set style sheet for sked catalog infos

--- skedcataloginfo.cpp
+++ skedcataloginfo.cpp
@@ -13,7 +13,7 @@ void SkedCatalogInfo::setFonts()
     const QFont fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
     QString family = fixedFont.family();
 
-    ui->textEdit_2->setStyleSheet(QString("font: \"%1\";").arg(family));
+    ui->textEdit_2->setFont(fixedFont);
     ui->textEdit_3->setFont(fixedFont);
     ui->textEdit_4->setFont(fixedFont);
 }
