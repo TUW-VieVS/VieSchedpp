1dcbb1256c9e55f5802caf8a874c24d0b1e20c8a
Matthias Schartner <matthias.schartner@geo.tuwien.ac.at>
2019-07-02 08:24:11 +0200

    bugfix cable wrap for stations with low azimuth span

--- Station/CableWrap/AbstractCableWrap.cpp
+++ Station/CableWrap/AbstractCableWrap.cpp
@@ -96,14 +96,15 @@ pair<double, double> AbstractCableWrap::getLimits( char section ) const {
 
 
 bool AbstractCableWrap::axisInsideCableWrap( double ax1, double ax2 ) const noexcept {
-    if (((axis1Up_ - axis1UpOffset_) - (axis1Low_ + axis1LowOffset_)) < 2 * pi) {
+    if ( ( ( axis1Up_ - axis1UpOffset_ ) - ( axis1Low_ + axis1LowOffset_ ) ) < 2 * pi ) {
         double ax1_1 = fmod( axis1Low_ + axis1LowOffset_, twopi );
         double ax1_2 = fmod( axis1Up_ - axis1UpOffset_, twopi );
 
         // over zero point or not
         if ( ax1_2 < ax1_1 ) {
             // over zero point
-            if ( ax1 < ax1_2 && ax1 > ax1_2 || ax2 < axis2Low_ + axis2LowOffset_ || ax2 > axis2Up_ - axis2UpOffset_ ) {
+            if ( ( ax1 < ax1_1 && ax1 > ax1_2 ) || ax2 < axis2Low_ + axis2LowOffset_ ||
+                 ax2 > axis2Up_ - axis2UpOffset_ ) {
                 return false;
             }
 
